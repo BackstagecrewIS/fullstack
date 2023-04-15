67360cf882089e6eecaef61401ea32fcce194813
BackstagecrewIS <locksmith@backstagecrew.com>
2023-04-15 20:21:12 +0000

    Add test_models.py

--- todo/test_models.py
+++ todo/test_models.py
@@ -1,23 +1,9 @@
 from django.test import TestCase
+from .models import Item
 
 
-# class TestViews(TestCase):
+class TestModels(TestCase):
 
-#     def test_get_todo_list(self):
-#         response = self.client.get('/')
-#         self.assertEqual(response.status_code, 200)
-#         self.assertTemplateUsed(response, 'todo/todo_list.html')
-
-#     def test_get_add_item_page(self):
-#         response = self.client.get('/add_item')
-#         self.assertEqual(response.status_code, 200)
-#         self.assertTemplateUsed(response, 'todo/add_item.html')
-
-    # def test_get_edit_item_page(self):
-
-    # def test_can_add_item(self):
-
-    # def test_can_delete_item(self):
-
-    # def test_can_toggle_item(self):
-
+    def test_done_defaults_to_false(self):
+        item = Item.objects.create(name='Test Todo Item')
+        self.assertFalse(item.done)
