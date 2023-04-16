892390ba84b85b5e0c41b9c8b3864b635b684d07
BackstagecrewIS <locksmith@backstagecrew.com>
2023-04-16 12:44:24 +0000

    Error correction

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -11,8 +11,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-from pathlib import Path
+import os
 import dj_database_url
+
+if os.path.isfile("env.py"):
+    import env
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,12 +26,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cue22sr7vyomy07$h1uqh$9wvn%orh@_3mlp_2j^i(fs&d!x!8'  # noqa
+# SECRET_KEY = 'django-insecure-cue22sr7vyomy07$h1uqh$9wvn%orh@_3mlp_2j^i(fs&d!x!8'  # noqa
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bsc-django-todo-app.herokuapp.com']
 
 
 # Application definition
@@ -85,9 +88,8 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # }
 
 DATABASES = {
-   'default': dj_database_url.parse('postgres://hvoupuny:X1856yzmJ1gklWjQu7xrWruHiXyFcjEC@kandula.db.elephantsql.com/hvoupuny')  # NOQA
+   'default': dj_database_url.parse('django-insecure-cue22sr7vyomy07$h1uqh$9wvn%orh@_3mlp_2j^i(fs&d!x!8')
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
